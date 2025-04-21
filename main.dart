54d8f5e93d700712d6322f3cf3024847c7252ca0
yasereljareeri <yasereljareeri@outlook.com>
2025-04-21 22:19:18 +0300

    update

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main() {
+  String myname = "moawiakhdair";
+
+  int myage = 23;
+
+  double myhighest = -181;
+
+  int myyear = 2002;
+
+  print(myname[0].toUpperCase());
+
+  print(myhighest.ceil());
+  print(myhighest.floor());
+  print(myhighest.round());
+  print(myhighest.isNegative);
+  print(myhighest.abs());
+
+  int myageAfter10Year = myage + 10;
+  print("my age after 10 years is $myageAfter10Year");
+}
